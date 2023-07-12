e087ad35d9bfccf0d65cafa6e430c30a70f52d00
Sunil Prajapati <scherbatsky.jr@Sunils-MacBook-Pro.local>
2023-07-12 11:12:52 +0545

    feat: mobile view for menu component

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,22 +1,49 @@
-import React from "react";
-import styles from "@/assets/css/components/menu.module.css";
+'use client';
+
+import React, { useState } from "react";
+
+import { Icon } from "@iconify/react";
+
+import styles from "@/assets/css/components/menu.module.scss";
 
 const Menu = () => {
+    const [showDropdown, setShowDropdown] = useState(false);
+
+    const onMenuClick = () => {
+        setShowDropdown(!showDropdown)
+    }
+    
     return (
         <div className={styles.menu}>
-            <div className={styles.menuImage}>
-                <img
-                    className={styles.image}
-                    src="@/assets/images/sunil.jpg"
-                />
-                <p className={styles.name}>Sunil Prajapati</p>
+            <div className={styles.menuBar}>
+                <div className={styles.menuImage}>
+                    <img
+                        className={styles.image}
+                        src="@/assets/images/sunil.jpg"
+                    />
+                    <p className={styles.name}>Sunil Prajapati</p>
+                </div>
+                <div className={styles.menuList}>
+                    <a className={styles.menuItem} href="">Home</a>
+                    <a className={styles.menuItem} href="#about">About</a>
+                    <a className={styles.menuItem} href="#projects">Projects</a>
+                    <a className={styles.menuItem} href="#contact">Contact</a>
+                </div>
+                <div className={styles.menuButton}>
+                    <button onClick={onMenuClick}>
+                        <Icon icon={showDropdown ? "charm:cross" : "cil:hamburger-menu"} />
+                    </button>
+                </div>
             </div>
-            <div className={styles.menuList}>
-                <a className={styles.menuItem} href="">Home</a>
-                <a className={styles.menuItem} href="#about">About</a>
-                <a className={styles.menuItem} href="#projects">Projects</a>
-                <a className={styles.menuItem} href="#contact">Contact</a>
-            </div>
+            {
+                showDropdown &&
+                    <div className={styles.menuDropdown}>
+                        <a className={styles.menuItem} href="">Home</a>
+                        <a className={styles.menuItem} href="#about">About</a>
+                        <a className={styles.menuItem} href="#projects">Projects</a>
+                        <a className={styles.menuItem} href="#contact">Contact</a>
+                    </div>
+            }
         </div>
     )
 }
